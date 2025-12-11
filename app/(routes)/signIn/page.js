7d58b605b311627baeb1774999a2ccd85a8fa2f2
KylesterChau <Kylethompsonchauder@gmail.com>
2025-12-10 21:19:24 -0700

    Set up backend firebase

--- app/(routes)/signIn/page.js
+++ app/(routes)/signIn/page.js
@@ -2,10 +2,20 @@ import Header from "../../components/head";
 import Footer from "../../components/foot";
 
 export default function signIn() {
+    const [signState, setSignState] = useState("Sign In");
+    
     return(
     <div className="min-h-screen bg-[#0F172A] text-white font-inter">
         <Header/>
         <h1>Sign in Page</h1>
+        {
+            (signState === "Sign In") ? (
+                <button onClick={() => setSignState("Sign Up")}>Switch to Sign Up</button>
+            ) : (
+                <button onClick={() => setSignState("Sign In")}>Switch to Sign In</button>
+            )
+        }
+
         <Footer/>
     </div>
     )
